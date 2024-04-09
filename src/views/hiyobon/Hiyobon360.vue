4077b10d9f0629231b52f7b659a565a503924172
turaboy <turaboy.holmirzaev@toptal.com>
2024-04-09 14:29:05 +0800

    updated hiyobon 360

--- src/views/hiyobon/Hiyobon360.vue
+++ src/views/hiyobon/Hiyobon360.vue
@@ -1,36 +1,82 @@
 <template>
-    <div style="height: 100%;">
-      <iframe
-        ref="iframe"
-        :src="iframeSrc"
-        width="100%"
-        height="100%"
-        frameborder="0"
-        scrolling="yes"
-      ></iframe>
+  <div style="position: relative; height: 100%;">
+    <div class="hide-text">
+      ADIBLAR HIYOBONI
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        iframeSrc: "https://uzbekistan360.uz/ru/location/adiblar-khijoboniaag"
-      };
-    },
-    methods: {
-      reloadIframe() {
-        // Reload iframe content
-        this.$refs.iframe.src = this.iframeSrc;
-      }
-    },
-    mounted() {
-      // You can reload iframe content after a certain time or event if needed
-      // For example, reload iframe content after 10 seconds
-      // setTimeout(() => {
-      //   this.reloadIframe();
-      // }, 10000);
+    <iframe
+      ref="iframe"
+      :src="iframeSrc"
+      width="100%"
+      height="100%"
+      frameborder="0"
+      scrolling="yes"
+    ></iframe>
+    <div class="scrolling-text"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      iframeSrc: "https://uzbekistan360.uz/ru/location/adiblar-khijoboniaag"
+    };
+  },
+  methods: {
+    reloadIframe() {
+      // Reload iframe content
+      this.$refs.iframe.src = this.iframeSrc;
     }
-  };
-  </script>
-  
+  },
+  mounted() {
+    // Initialization code can go here
+  }
+};
+</script>
+
+<style>
+.scrolling-text {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  background-color: rgba(0,151,167, 0.7); /* Semi-transparent black background */
+  color: white; /* White text color */
+  white-space: nowrap;
+  overflow: hidden;
+  padding: 10px 0; /* Some padding to ensure text is vertically centered */
+}
+
+.hide-text {
+  position: absolute;
+  top: 0;
+  width: 100%;
+  background-color: #00838F; /* Semi-transparent black background */
+  color: white; /* White text color */
+  white-space: nowrap;
+  overflow: hidden;
+  padding: 10px 0; /* Some padding to ensure text is vertically centered */
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 24px;
+  line-height: 1.8;
+
+}
+
+.scrolling-text::after {
+  content: 'https://uzbekistan360.uz | Adiblar hiyoboni 360 saytidan olindi';
+  display: inline-block;
+  padding-left: 100%;
+  animation: scroll 20s linear infinite;
+}
+
+@keyframes scroll {
+  0% {
+    transform: translateX(100%);
+  }
+  100% {
+    transform: translateX(-100%);
+  }
+}
+</style>
